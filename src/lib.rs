301e5080aed8490d286e72acbfb1d4c3d7011b20
Mark LeMoine <linclelinkpart5@gmail.com>
2021-01-18 23:42:32 -0800

    Add `scale_amp`/`add_amp`/`mul_amp` benchmarks

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,33 @@ use dasp::Frame;
 #[bench]
 fn bench_frame_offset_amp(b: &mut Bencher) {
     b.iter(|| {
-        let fa = [-0.50, -0.25, 0.00, 0.25, 0.5];
+        let fa = [-0.500, -0.250, 0.000, 0.250, 0.500];
         fa.offset_amp(0.5)
+    });
+}
+
+#[bench]
+fn bench_frame_scale_amp(b: &mut Bencher) {
+    b.iter(|| {
+        let fa = [-0.500, -0.250, 0.000, 0.250, 0.500];
+        fa.scale_amp(0.5)
+    });
+}
+
+#[bench]
+fn bench_frame_add_amp(b: &mut Bencher) {
+    b.iter(|| {
+        let fa = [-0.500, -0.250, 0.000, 0.250, 0.500];
+        let fb = [0.250, 0.125, 0.000, -0.125, -0.250];
+        fa.add_amp(fb)
+    });
+}
+
+#[bench]
+fn bench_frame_mul_amp(b: &mut Bencher) {
+    b.iter(|| {
+        let fa = [-0.500, -0.250, 0.000, 0.250, 0.500];
+        let fb = [0.250, 0.125, 0.000, -0.125, -0.250];
+        fa.mul_amp(fb)
     });
 }
